c91c4d70e545be62465b305397575233721560cc
AmitKuMaurya <amit69maurya69@gmail.com>
2022-12-28 19:55:24 +0530

    chanded color of hover bg

--- src/Components/gitStats/Stats.jsx
+++ src/Components/gitStats/Stats.jsx
@@ -5,51 +5,51 @@ import "./Stats.css";
 
 function Stats() {
 
-    const selectLastHalfYear = (contributions) => {
-        const currentYear = new Date().getFullYear();
-        const currentMonth = new Date().getMonth();
-        const shownMonths = 12;
-    
-        return contributions.filter((day) => {
-          const date = new Date(day.date);
-          const monthOfDay = date.getMonth();
-    
-          return (
-            date.getFullYear() === currentYear &&
-            monthOfDay > currentMonth - shownMonths &&
-            monthOfDay <= currentMonth
-          );
-        });
-      };
+  const selectLastHalfYear = (contributions) => {
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth();
+    const shownMonths = 12;
+
+    return contributions.filter((day) => {
+      const date = new Date(day.date);
+      const monthOfDay = date.getMonth();
+
+      return (
+        date.getFullYear() === currentYear &&
+        monthOfDay > currentMonth - shownMonths &&
+        monthOfDay <= currentMonth
+      );
+    });
+  };
 
   return (
     <div>
-    <section id='stats'>
-        <h2 >DAYS I CODE</h2>   
-        <div className = 'container__calendar'>
+      <section id='stats'>
+        <h2 >DAYS I CODE</h2>
+        <div className='container__calendars'>
 
-        <GitHubCalendar 
-        
-        // style={{ margin: "auto" }}
-        transformData={selectLastHalfYear}
-        username='AmitKuMaurya'
-        // blockSize={20}
-        // fontSize = {15}
-        >
-            <ReactToolTip delayShow={10} html/>
-        </GitHubCalendar>
+          <GitHubCalendar
 
-            </div>
+            // style={{ margin: "auto" }}
+            transformData={selectLastHalfYear}
+            username='AmitKuMaurya'
+            blockSize={13}
+            fontSize={15}
+          >
+            <ReactToolTip delayShow={10} html />
+          </GitHubCalendar>
+
+        </div>
         <div className='container__stats'>
-            <div>
-                <img src="https://github-readme-stats.vercel.app/api?username=AmitKuMaurya&theme=default&hide_border=false&include_all_commits=false&count_private=true" alt="" />
-            </div>
-            <div>
-                <img src="https://github-readme-streak-stats.herokuapp.com/?user=AmitKuMaurya&theme=default&hide_border=false" alt="" />
-            </div>
+          <div>
+            <img src="https://github-readme-stats.vercel.app/api?username=AmitKuMaurya&theme=default&hide_border=false&include_all_commits=false&count_private=true" alt="" />
+          </div>
+          <div>
+            <img src="https://github-readme-streak-stats.herokuapp.com/?user=AmitKuMaurya&theme=default&hide_border=false" alt="" />
+          </div>
         </div>
 
-    </section>
+      </section>
     </div>
   )
 }
